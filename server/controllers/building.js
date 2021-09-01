a2a7fefd14cecd6801b56dbf5f5673c6cfecd1a6
bonino97 <juanbonino97@gmail.com>
2021-09-01 20:51:28 -0300

    Fix building.

---
+++ server/controllers/building.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+exports.getAllBuildings = async (req, res) => {
+  try {
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+exports.getBuildingById = async (req, res) => {
+  try {
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+exports.getBuildingByCategory = async (req, res) => {
+  try {
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+exports.deleteBuilding = async (req, res) => {
+  try {
+  } catch (error) {
+    console.error(error);
+  }
+};
