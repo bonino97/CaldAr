34b83f364f9a1b119d319aba401cbc46583b0cf6
rubensato <rubensato@gmail.com>
2021-09-02 07:52:17 -0300

    getBoilerById controller code

--- server/controllers/boiler.js
+++ server/controllers/boiler.js
@@ -13,11 +13,25 @@ exports.getAllBoilers = async (req, res) => {
 
 exports.getBoilerById = async (req, res) => {
   try {
-      
+    const boilerId = req.query.boilerId;
+    let boilerJSON = fs.readFileSync('data/boilers.json', 'utf8');
+    let boilers = JSON.parse(boilerJSON);
+
+    let boiler = boilers.filter(
+      (boiler) => Number(boiler.id) === Number(boilerId)
+    );
+
+    if (boiler.length === 0)
+      return res.status(400).json('No se encontro una caldera con ese Id.');
+
+    return res.status(200).json(boiler);
   } catch (error) {
     console.error(error);
   }
 };
+
+
+
 
 exports.getBoilerByCategory = async (req, res) => {
   try {
