6c439b40a475e9ce204f69ca144364cd9732a38f
rubensato <rubensato@gmail.com>
2021-09-02 07:38:35 -0300

    getAllBoilers controller code

--- server/controllers/boiler.js
+++ server/controllers/boiler.js
@@ -1,15 +1,19 @@
 const fs = require('fs');
 
-exports.getBoilerById = async (req, res) => {
+exports.getAllBoilers = async (req, res) => {
   try {
-      
+    let boilerJSON = fs.readFileSync('data/boilers.json', 'utf8');
+    let boilers = JSON.parse(boilerJSON);
+    if (!boilers) return res.status(400).json('Json inexistente.');
+    return res.status(200).json(boilers);
   } catch (error) {
     console.error(error);
   }
 };
 
-exports.getAllBoilers = async (req, res) => {
+exports.getBoilerById = async (req, res) => {
   try {
+      
   } catch (error) {
     console.error(error);
   }
