629d6fb5ec3fb45f7e64636ac99ff0920e911aad
rubensato <rubensato@gmail.com>
2021-09-08 20:20:59 -0300

    routes edition

--- server/routes/boiler.js
+++ server/routes/boiler.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const boilerController = require('../controllers/boiler');
 
 module.exports = () => {
-  router.get('/:boilerId', boilerController.getBoilerById);
-  router.get('/', boilerController.getAllBoilers);
+  router.get('/id/:boilerId', boilerController.getBoilerById);
+  router.get('/all', boilerController.getAllBoilers);
   router.get('/type/:boilerType', boilerController.getBoilerByType);
 
   router.delete('/:id', boilerController.deleteBoiler);
