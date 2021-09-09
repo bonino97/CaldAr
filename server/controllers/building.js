bafa24ce72b8b8edc5f71aec60f73d0e4322b007
bonino97 <juanbonino97@gmail.com>
2021-09-08 22:58:47 -0300

    Finishing logic for update, add, and delete buildings

--- server/controllers/building.js
+++ server/controllers/building.js
@@ -34,7 +34,7 @@ exports.getBuildingById = async (req, res) => {
 
 exports.getBuildingByCategory = async (req, res) => {
   try {
-    const category = req.params.category;
+    const category = req.params.category === 'true' ? true : false;
     let buildingJSON = fs.readFileSync('data/buildings.json', 'utf8');
     let buildings = JSON.parse(buildingJSON);
     let buildingsCategory = buildings.filter(
@@ -52,6 +52,110 @@ exports.getBuildingByCategory = async (req, res) => {
 
 exports.deleteBuilding = async (req, res) => {
   try {
+    const buildingId = req.params.buildingId;
+
+    let buildingJSON = fs.readFileSync('data/buildings.json', 'utf8');
+
+    let buildings = JSON.parse(buildingJSON);
+    if (!buildings) return res.status(400).json('Json Inexistente.');
+
+    let buildingIndex = buildings.findIndex(
+      (building) => Number(building.id) === Number(buildingId)
+    );
+
+    if (buildingIndex === -1) {
+      return res
+        .status(400)
+        .send({ error: `No existe el edificio con id: ${buildingId} .` });
+    }
+
+    buildings.splice(buildingIndex, 1);
+
+    fs.writeFileSync('data/buildings.json', JSON.stringify(buildings), {
+      encoding: 'utf8',
+      flag: 'w',
+    });
+
+    return res.status(200).json(buildings);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json('Internal server error.');
+  }
+};
+
+exports.addNewBuilding = async (req, res) => {
+  try {
+    const { address, category } = req.body;
+
+    let buildingJSON = fs.readFileSync('data/buildings.json', 'utf8');
+    let buildings = JSON.parse(buildingJSON);
+    if (!buildings) return res.status(400).json('Json Inexistente.');
+
+    if (!address) {
+      return res.status(400).send({ error: 'No existe la direccion.' });
+    }
+
+    if (!category) {
+      return res.status(400).send({ error: 'No existe la categoria.' });
+    }
+
+    const buildingId = Number(buildings[buildings.length - 1].id) + 1;
+    const newBuilding = { id: buildingId, address, category };
+    buildings.push(newBuilding);
+
+    fs.writeFileSync('data/buildings.json', JSON.stringify(buildings), {
+      encoding: 'utf8',
+      flag: 'w',
+    });
+
+    return res.status(200).json(buildings);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json('Internal server error.');
+  }
+};
+
+exports.updateBuilding = async (req, res) => {
+  try {
+    const { buildingId, address, category } = req.body;
+
+    let buildingJSON = fs.readFileSync('data/buildings.json', 'utf8');
+
+    let buildings = JSON.parse(buildingJSON);
+    if (!buildings) return res.status(400).json('Json Inexistente.');
+
+    let buildingIndex = buildings.findIndex(
+      (building) => Number(building.id) === Number(buildingId)
+    );
+
+    if (buildingIndex === -1) {
+      return res
+        .status(400)
+        .send({ error: `No existe el edificio con id: ${buildingId} .` });
+    }
+
+    if (!address) {
+      return res.status(400).send({ error: 'No existe la direccion.' });
+    }
+
+    if (!category) {
+      return res.status(400).send({ error: 'No existe la categoria.' });
+    }
+
+    const updatedBuilding = {
+      id: Number(buildingId),
+      address,
+      category: category === 'true' ? true : false,
+    };
+
+    buildings[buildingIndex] = updatedBuilding;
+
+    fs.writeFileSync('data/buildings.json', JSON.stringify(buildings), {
+      encoding: 'utf8',
+      flag: 'w',
+    });
+
+    return res.status(200).json(buildings);
   } catch (error) {
     console.error(error);
     return res.status(500).json('Internal server error.');
--- server/routes/building.js
+++ server/routes/building.js
@@ -4,9 +4,13 @@ const buildingController = require('../controllers/building');
 
 module.exports = () => {
   router.get('/all', buildingController.getAllBuildings);
-  router.get('/:buildingId', buildingController.getBuildingById);
   router.get('/category/:category', buildingController.getBuildingByCategory);
+  router.get('/:buildingId', buildingController.getBuildingById);
 
-  router.delete('/:id', buildingController.deleteBuilding);
+  router.post('/', buildingController.addNewBuilding);
+
+  router.put('/', buildingController.updateBuilding);
+
+  router.delete('/:buildingId', buildingController.deleteBuilding);
   return router;
 };
