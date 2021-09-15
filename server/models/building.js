51326fa304a1cdab84e6926f0d5301692eb2e304
bonino97 <juanbonino97@gmail.com>
2021-09-15 20:50:42 -0300

    Finishing CRUD

--- server/models/building.js
+++ server/models/building.js
@@ -4,11 +4,14 @@ const buildingSchema = new mongoose.Schema({
   address: {
     type: String,
     required: 'La direccion es requerida.',
+    trim: true,
   },
   category: {
     type: String,
     required: 'La categoria es requerida.',
+    trim: true,
   },
+  description: String,
 });
 
 module.exports = mongoose.model('building', buildingSchema);
