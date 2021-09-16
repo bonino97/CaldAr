a5390eac6b1e267eb80b58170f5c61d808672235
walfonso <walfonso@hotmail.com>
2021-09-16 01:55:28 -0300

    User || Add - Update - Delete

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,5 +1,58 @@
-const fs = require('fs');
+const User = require('../models/user');
 
+// Agregar nuevo Usuario
+exports.addNewUser = async (req, res) => {
+  try {
+    const body = req.body;
+    const user = new User(body);
+
+    if (!user) return res.status(400).json('Error al crear el usuario.');
+
+    await user.save();
+    return res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+// Actualizar Usuario
+exports.updateUser = async (req, res) => {
+  try {
+    const body = req.body;
+
+    if (!body.userId)
+      return res.status(400).json('No existe usuario con ese Id.');
+
+    const user = await User.findByIdAndUpdate(body.userId, body, {
+      new: true,
+    });
+
+    if (!user) return res.status(400).json('Error al actualizar el usuario.');
+
+    return res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+// Eliminar usuario
+exports.deleteUser = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const user = await User.findByIdAndDelete(userId);
+
+    if (!user) return res.status(400).json('Error al eliminar el usuario.');
+
+    return res.status(200).json('El usuario ha sido eliminado correctamente.');
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+//
 exports.getAllUsers = async (req, res) => {
   try {
     let usersJSON = fs.readFileSync('data/users.json', 'utf8');
@@ -48,122 +101,5 @@ exports.getUserByDepartment = async (req, res) => {
     return res.status(200).json(usersDepartment);
   } catch (error) {
     console.error(error);
-  }
-};
-
-exports.deleteUser = async (req, res) => {
-  try {
-    const userId = req.params.userId;
-    let usersJSON = fs.readFileSync('data/users.json', 'utf8');
-    let users = JSON.parse(usersJSON);
-
-    let userIndex = users.findIndex(
-      (user) => Number(user.id) === Number(userId)
-    );
-
-    if (userIndex === -1) {
-      return res.status(400).send({ error: `No existe el Id: ${userId}.` });
-    }
-    users.splice(userIndex, 1);
-    fs.writeFileSync('data/users.json', JSON.stringify(users), {
-      encoding: 'utf8',
-      flag: 'w',
-    });
-
-    return res.status(200).json(users);
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json('Error Interno del Servidor.');
-  }
-};
-
-//
-exports.addNewUser = async (req, res) => {
-  try {
-    const { first_name, last_name, email, department } = req.body;
-    console.log(req.body);
-    let usersJSON = fs.readFileSync('data/users.json', 'utf8');
-    let users = JSON.parse(usersJSON);
-
-    if (!first_name) {
-      return res.status(400).send({ error: 'No existe el nombre.' });
-    }
-
-    if (!last_name) {
-      return res.status(400).send({ error: 'No existe el apellido.' });
-    }
-    if (!email) {
-      return res.status(400).send({ error: 'No existe el email.' });
-    }
-
-    if (!department) {
-      return res.status(400).send({ error: 'No existe el departamento.' });
-    }
-
-    const userId = Number(users[users.length - 1].id) + 1;
-    const newUser = { id: userId, first_name, last_name, email, department };
-    users.push(newUser);
-
-    fs.writeFileSync('data/users.json', JSON.stringify(users), {
-      encoding: 'utf8',
-      flag: 'w',
-    });
-
-    return res.status(200).json(users);
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json('Internal server error.');
-  }
-};
-
-//
-exports.updateUser = async (req, res) => {
-  try {
-    const { userId, first_name, last_name, email, department } = req.body;
-    console.log(userId);
-    let usersJSON = fs.readFileSync('data/users.json', 'utf8');
-    let users = JSON.parse(usersJSON);
-    let userIndex = users.findIndex(
-      (user) => Number(user.id) === Number(userId)
-    );
-
-    if (userIndex === -1) {
-      return res.status(400).send({ error: `No existe el Id: ${userId}.` });
-    }
-
-    if (!first_name) {
-      return res.status(400).send({ error: 'No existe el nombre.' });
-    }
-
-    if (!last_name) {
-      return res.status(400).send({ error: 'No existe el apellido.' });
-    }
-    if (!email) {
-      return res.status(400).send({ error: 'No existe el email.' });
-    }
-
-    if (!department) {
-      return res.status(400).send({ error: 'No existe el departamento.' });
-    }
-
-    const updateUser = {
-      id: Number(userId),
-      first_name,
-      last_name,
-      email,
-      department,
-    };
-
-    users[userIndex] = updateUser;
-
-    fs.writeFileSync('data/users.json', JSON.stringify(users), {
-      encoding: 'utf8',
-      flag: 'w',
-    });
-
-    return res.status(200).json(users);
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json('Internal server error.');
   }
 };
