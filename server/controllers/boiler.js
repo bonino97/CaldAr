6dc06a582879b0932222e597aa1282cd14ccd41a
rubensato <rubensato@gmail.com>
2021-09-15 21:53:01 -0300

    start boiler controller edition (CRUD)

--- server/controllers/boiler.js
+++ server/controllers/boiler.js
@@ -38,63 +38,6 @@ exports.addNewBoiler = async (req, res) => {
 };
 
 
-// GET All Boilers
-exports.getAllBoilers = async (req, res) => {
-  try {
-    let boilerJSON = fs.readFileSync('data/boilers.json', 'utf8');
-    let boilers = JSON.parse(boilerJSON);
-    if (!boilers) return res.status(400).json('Json inexistente.');
-    return res.status(200).json(boilers);
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-
-// GET A Boiler By ID
-exports.getBoilerById = async (req, res) => {
-  try {
-    const boilerId = req.params.boilerId;
-    let boilerJSON = fs.readFileSync('data/boilers.json', 'utf8');
-    let boilers = JSON.parse(boilerJSON);
-
-    let boiler = boilers.filter(
-      (boiler) => Number(boiler.id) === Number(boilerId)
-    );
-
-    if (boiler.length === 0)
-      return res.status(400).json('No se encontro una caldera con ese Id.');
-
-    return res.status(200).json(boiler);
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-
-// GET A Boiler By Type (A, B, C, or D)
-exports.getBoilerByType = async (req, res) => {
-  try {
-    const boilerType = req.params.boilerType;
-    let boilerJSON = fs.readFileSync('data/boilers.json', 'utf8');
-    let boilers = JSON.parse(boilerJSON);
-
-    let boiler = boilers.filter(
-      (boiler) => boiler.type === boilerType
-    );
-
-    if (boiler.length === 0)
-      return res.status(400).json(`No se encontraron calderas del tipo ${boilerType}`);
-
-    return res.status(200).json(boiler);
-
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json('Internal server error.');
-  }
-};
-
-
 // UPDATE Or MODIFY A Boiler By ID
 exports.updateBoiler = async (req, res) => {
   try {
@@ -174,6 +117,63 @@ exports.deleteBoiler = async (req, res) => {
     });
 
     return res.status(200).json(boilers);
+
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json('Internal server error.');
+  }
+};
+
+
+// GET All Boilers
+exports.getAllBoilers = async (req, res) => {
+  try {
+    let boilerJSON = fs.readFileSync('data/boilers.json', 'utf8');
+    let boilers = JSON.parse(boilerJSON);
+    if (!boilers) return res.status(400).json('Json inexistente.');
+    return res.status(200).json(boilers);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+
+// GET A Boiler By ID
+exports.getBoilerById = async (req, res) => {
+  try {
+    const boilerId = req.params.boilerId;
+    let boilerJSON = fs.readFileSync('data/boilers.json', 'utf8');
+    let boilers = JSON.parse(boilerJSON);
+
+    let boiler = boilers.filter(
+      (boiler) => Number(boiler.id) === Number(boilerId)
+    );
+
+    if (boiler.length === 0)
+      return res.status(400).json('No se encontro una caldera con ese Id.');
+
+    return res.status(200).json(boiler);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+
+// GET A Boiler By Type (A, B, C, or D)
+exports.getBoilersByType = async (req, res) => {
+  try {
+    const boilerType = req.params.boilerType;
+    let boilerJSON = fs.readFileSync('data/boilers.json', 'utf8');
+    let boilers = JSON.parse(boilerJSON);
+
+    let boiler = boilers.filter(
+      (boiler) => boiler.type === boilerType
+    );
+
+    if (boiler.length === 0)
+      return res.status(400).json(`No se encontraron calderas del tipo ${boilerType}`);
+
+    return res.status(200).json(boiler);
 
   } catch (error) {
     console.error(error);
